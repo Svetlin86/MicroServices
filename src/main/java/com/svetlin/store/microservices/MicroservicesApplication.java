c65f42a3f091a865af5fd048e0ede5e79c252bc5
Svetlin86 <93228248+Svetlin86@users.noreply.github.com>
2022-12-14 13:12:34 +0200

    Start1

--- src/main/java/com/svetlin/store/microservices/MicroservicesApplication.java
+++ src/main/java/com/svetlin/store/microservices/MicroservicesApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MicroservicesApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(MicroservicesApplication.class, args);
+		System.out.println("Hello");
 	}
 
 }
