8511613c6a8f6067525e239fe2a8644ee3a2ea83
Svetlin86 <93228248+Svetlin86@users.noreply.github.com>
2022-12-15 11:02:00 +0200

    Fixes

---
+++ PersonManagement/src/main/java/com/svetlin/store/person/controller/PersonController.java
@@ -0,0 +1,8 @@
+package com.svetlin.store.person.controller;
+
+import org.springframework.data.rest.webmvc.BasePathAwareController;
+
+@BasePathAwareController
+public class PersonController {
+
+}
